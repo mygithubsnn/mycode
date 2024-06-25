8b4c292e0520697a5141e4b41b5bc9916f1679b0
mygithubsnn <snn305@gmail.com>
2024-06-25 15:37:11 +0000

    your commit message

--- montypython/monty_python2.py
+++ montypython/monty_python2.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 
-## The objective of lab is to write a simple guessing program utilizing conditionals. Our simple program will behave in the following manner:
-
-##Ask the user to, Finish the movie title, "Monty Python's The Life of ..."
-##Collects up to 3 responses from the user
-##Responses to the user's input should include: "Correct!", "Sorry, try again!", and "Sorry, the answer was Brian."##
+"""" while and if loop n"""
 
 
 respons = " "
 count = 0 
 
-while count < 3 and respons != "Brian":
+while count < 3 and respons != "brian":
     count += 1
     respons = input("please enter your guess: ")
-    if respons == "Brian":
+    if respons.lower() == "brian":
         print("Correct!")
         break
     elif count == 3:
